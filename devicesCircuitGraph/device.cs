20d06914837c2767214638dfbcf625a50be21801
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-03-09 17:54:35 +0200

    Add project files.

---
+++ devicesCircuitGraph/device.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace devicesCircuitGraph
+{
+    public partial class device : UserControl
+    {
+        public device()
+        {
+            InitializeComponent();
+        }
+        public void setLabel(string s)
+        { 
+            this.label1.Text = s;
+        }
+            
+        private void device_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
